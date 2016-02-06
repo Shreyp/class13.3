f8403bba0b76e003797ee0b378c30a7ee579d56e
unknown <you@example.com>
2016-02-06 14:06:11 -0500

    added some stuff

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var GitHubApi = require("node-github");
+
+
 var app = express();
 var PORT = process.env.PORT || 7000;
 
@@ -8,6 +11,29 @@ app.use("/css", express.static("public/css"));
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
+
+app.get("/register", function(req, res) {
+  res.sendFile(process.cwd() + "/views/register.html");
+});
+
+app.get("/github/:user", function(req, res) {
+  var github = new GitHubApi({
+      version: "3.0.0"
+  });
+  var user = req.params.user;
+  github.user.getFrom({
+    user: user
+  }, function(err, gitResponse){
+    res.send(gitResponse);
+  });
+})
+
+app.get("/dashboard", function(req, res) {
+  res.sendFile(process.cwd() + "/views/dashboard.html");
+});
+
+  
+
 
 app.listen(PORT, function() {
   console.log("Listening on port %s", PORT);
